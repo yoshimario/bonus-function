fdc33091c5671f1213c37d4e3eb09fed5efa511a
Sean Wilson <paintballsean@gmail.com>
2020-10-19 11:43:49 -0700

    Commit adds last letter and first letter capital.
    Co-authored-by: Joe Nero <josephnero111@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-const sentencePromp = prompt("Please enter a sentence:");
-alert(sentencePromp);
-function capital ();
+const sentencePrompt = prompt("Please enter a sentence:");
+alert(sentencePrompt);
+
+function capital (sentencePrompt) {
+
+	let finalSentence = sentencePrompt.charAt(0).toUpperCase() + sentencePrompt.slice(1);
+  
+  let lastLetter =  finalSentence.charAt(finalSentence.length-1).toUpperCase();
+  
+return lastLetter; 
+ 
+};
+
+let finalSentence = capital (sentencePrompt);
+
+alert(finalSentence);
