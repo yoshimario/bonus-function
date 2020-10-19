0812bbf7e8df38329d1d3c3d7ae3a4a7e6887453
Sean Wilson <paintballsean@gmail.com>
2020-10-19 14:15:52 -0700

    commit adds part of first and last character only shown.
    Co-authored-by: Joe Nero <josephnero111@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,42 @@ return test2;
  
 };
 
-let finalSentence = capital (sentencePrompt);
+let finalSentence = capital(sentencePrompt); 
 
-alert(finalSentence);
+alert(finalSentence); 
+
+function swap (string) {
+
+let first=string.charAt(0);
+let last=string.charAt(string.length-1);
+
+let newstring = last + string.slice(1);
+let newstring2 = newstring.substring(0,newstring.length-1);
+let newstring3= newstring2 + first; 
+
+
+return newstring3; 
+
+}
+
+let output = swap (finalSentence);
+alert(output); 
+
+function firstLastChar(string) {
+
+	let firstChar = capital(string).charAt(0);
+  let lastChar = swap(string).charAt(0); 
+  let firstCap = firstChar + lastChar; 
+  let lastCap = lastChar + firstChar; 
+  
+  return [firstCap, lastCap]; 
+
+ // let firstChar = string.charAt(0);
+ // let lastChar = string.charAt(-1);
+ // let sentChar = firstChar + lastChar;
+ // return sentChar;
+}
+
+let [firstLastSentChar, lastFirstSentChar] = firstLastChar(finalSentence); 
+alert(firstLastSentChar); 
+alert(lastFirstSentChar);
---
+++ ramb.js
@@ -0,0 +1,15 @@
+function swap (finalSentence) {
+
+  let test = finalSentence.charAt(0);
+  let test2 = finalSentence.charAt(finalSentence.length-1);
+  
+  let finalSentence = test2 + sentencePrompt.slice(1);
+  
+  return = finalSentence; 
+  
+   
+  };
+  
+  let test = swap(finalSentence); 
+  
+  alert(test);  
